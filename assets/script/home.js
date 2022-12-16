9f295cf0f0e788d918256bdf38942910e3f601a7
Gabriel <gnicholauson@gmail.com>
2022-12-16 11:08:01 -0600

    add comments

--- assets/script/home.js
+++ assets/script/home.js
@@ -8,7 +8,7 @@ getUsers();
  *  Functions
 ***********************/
 
-async function getUsers() {
+async function getUsers() { //pulls random users from api
     const url = 'https://randomuser.me/api/?nat=CA&results=10&seed=same';
     const options = {
         method: 'GET',
@@ -19,19 +19,21 @@ async function getUsers() {
     };
 
     try {
-        const result = await fetch(url, options);
+        const result = await fetch(url, options); //Fetch random users
 
-        if (result.status >= 200 && result.status < 400) {
+        if (result.status >= 200 && result.status < 400) { //If successful
             const data = await result.json();
-            displayFollowers(data.results);
+            displayFollowers(data.results); //Give results to displayFollowers function
         }
-    } catch(error) {
+    } catch(error) { //if there is an error
         console.log(error);
     }
 }
 
-function displayFollowers(followers) {
-    for (let i = 0; i < followers.length; i++) {
+function displayFollowers(followers) { //displays random users on page
+    for (let i = 0; i < followers.length; i++) { //goes through each user
+
+        //Displays users picture, first name and last name
         following.innerHTML += `<div class="user flexbox">
         <div class="pfp" style="background-image: url(${followers[i].picture.medium})"></div>
         <div class="user-info">
